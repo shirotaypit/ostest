54fa87a7841b0b2f650257cf239c84da6f5058d1
shirotaypit <shirotay.pit@gmail.com>
2020-07-29 07:32:17 +0000

    filename changed

--- zonbi.c
+++ zonbi.c
@@ -1,79 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <time.h>
 
-#define LENGTH (512UL*1024*1024)
-#define INTERVAL 5
+int main() {
+    int p_id;
+    int status;
+    int return_code = 0;
 
-
-
-static void write_bytes(char *addr)
-{
-	unsigned long i,count,point;
-
-	point = LENGTH / 20;
-	count = 0;
-	for (i = 0; i < LENGTH; i++){
-		*(addr + i) = (unsigned int)i;
-		count++;
-		if( count >= point ){
-			sleep(1);
-			count=0;
-		}
-	}
-	return;
-}
-
-static int read_bytes(char *addr)
-{
-	unsigned long i,count,point;
-
-	point = LENGTH / 20;
-	count = 0;
-	for (i = 0; i < LENGTH; i++){
-		(void)(addr + i);
-		count++;
-		if( count >= point ){
-			sleep(1);
-			count=0;
-		}
-	}	
-	return 0;
-}
-
-int main(void)
-{
-	void *addr;
-	int ret;
-
-	printf("メモリ管理\n");
-	sleep(INTERVAL);
-
-	printf("step1 プログラムでメモリをアロケート 512Mバイト\n");
-    addr = malloc(LENGTH);
-
-	sleep(INTERVAL);
-	printf("step2 読み込みを開始　何も変わらないはず\n");
-	ret = read_bytes(addr);
-	printf("step2 読み込みを終了\n");
-
-	sleep(INTERVAL);
-	printf("step3 書き込みを開始　usedが上昇し　availableが減少する\n");
-	write_bytes(addr);
-	printf("step3 書き込みを終了\n");
-
-	sleep(INTERVAL);
-	printf("step4 読み込みを開始　何も変わらないはず\n");
-	ret = read_bytes(addr);
-	printf("step4 読み込みを終了\n");
-
-	sleep(INTERVAL);
-	printf("step5 メモリを開放　usedが開放され availableが復活\n");
-	free(addr);
-
-	sleep(INTERVAL);
-	printf("メモリ管理　完\n");
-
-	return ret;
+    p_id = fork();
+    printf("process %d created\n",p_id);
+    if (p_id == 0 ) {
+        printf("process %d nothing to do\n",p_id);
+    }
+    else {
+        if (p_id != -1) {
+            printf("process %d sleep\n",p_id);
+            sleep(60);
+            return_code = 1;
+        }
+    }
+    printf("process %d end\n",p_id);
+    return return_code;
 }
